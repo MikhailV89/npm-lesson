0552e048055c83ecac0afe5cfe110777bf3d35dd
https://github.com/MikhailV89 <mvasilchenko89@gmail.com>
2024-03-06 15:19:02 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+const sum = function (a, b) {
+  return a + b;
+};
+
+sum(1, 2);
+const minus = function (a, b) {
+  return a - b;
+};
+
+minus(4, 2);
+const mult = function (a, b) {
+  return a * b;
+};
+
+mult(1, 2);
